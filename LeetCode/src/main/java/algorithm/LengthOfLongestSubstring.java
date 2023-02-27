65f75ac0d8a102be3e520f028e2298b4d01710f1
Mu Yu Lun <yulun.mu@bintools.cn>
2023-02-27 19:59:41 +0800

    3.lengthOfLongestSubstring LeetCode

---
+++ LeetCode/src/main/java/algorithm/LengthOfLongestSubstring.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.HashMap;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+
+        System.out.println(lengthOfLongestSubstring("abcdeacfg"));
+
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                start = Math.max(map.get(c) + 1, start);
+            }
+
+            max = Math.max(max, i - start + 1);
+            map.put(c, i);
+        }
+
+        return max;
+    }
+}
---
+++ LeetCode/src/main/java/sort/QuickSort.java
@@ -0,0 +1,61 @@
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,1,0,9,8,6,10,20,13};
+        System.out.println("快排之前：");
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr,0,arr.length-1);
+        System.out.println("\n快排之后：");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     *
+     * @param arr:待排序的数组
+     * @param low：左边低位指针
+     * @param high：右边高位指针
+     */
+    public static void quickSort(int[] arr,int low,int high){
+
+        if (low >= high) {
+            return;
+        }
+
+        int l = low;
+        int r = high;
+        int temp = 0;
+        int pivot = arr[low];
+
+        while (l < r) {
+            //先右遍历，需要判断arr[l=r] > arr[low]
+            while (l < r && arr[r] >= pivot) {
+                r--;
+            }
+
+            while (l < r && arr[l] <= pivot) {
+                l++;
+            }
+
+            if (l < r) {
+                temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+        }
+
+        arr[low] = arr[l];
+        arr[l] = pivot;
+
+        if (low < l) {
+            quickSort(arr, low, l - 1);
+        }
+
+        if (high > r) {
+            quickSort(arr, r + 1, high);
+        }
+
+    }
+}
