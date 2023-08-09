a5cf87fc4733b97ce37080a7d90bda37804e2e6a
Mylqsnh <jlthmylqsnh@163.com>
2023-08-09 20:25:48 +0800

    leetcode 92 206

---
+++ LeetCode/src/main/java/algorithm/linked/ReverseBetween_92.java
@@ -0,0 +1,35 @@
+package algorithm.linked;
+
+import java.util.Arrays;
+
+public class ReverseBetween_92 {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(Arrays.asList(1, 2, 3, 4, 5));
+        ListNode listNode1 = reverseBetween(listNode, 2, 4);
+        listNode1.print();
+    }
+
+    static ListNode successor = null;
+
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+         if (left == 1) {
+             return reverseN(head, right);
+         }
+         head.next = reverseBetween(head.next, left - 1, right - 1);
+         return head;
+    }
+
+    public static ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+           successor = head.next;
+           return head;
+        }
+
+        ListNode last = reverseN(head.next, n - 1);
+
+        head.next.next = head;
+        head.next = successor;
+
+        return last;
+    }
+}
---
+++ LeetCode/src/main/java/algorithm/linked/ReverseLis_206.java
@@ -0,0 +1,27 @@
+package algorithm.linked;
+
+import java.util.Arrays;
+
+public class ReverseLis_206 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(Arrays.asList(1, 2, 3, 4, 5));
+        ListNode listNode1 = reverseList(listNode);
+        listNode1.print();
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode last = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return last;
+    }
+
+}
