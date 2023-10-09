8940e744b433e9d73e50098a2b2ce04e039e6b42
Mylqsnh <jlthmylqsnh@163.com>
2023-10-09 18:06:19 +0800

    leetcode 167

---
+++ LeetCode/src/main/java/algorithm/array/TwoSum_167.java
@@ -0,0 +1,29 @@
+package algorithm.array;
+
+public class TwoSum_167 {
+
+    public static void main(String[] args) {
+        int[] a = {2, 7, 11, 15};
+        int[] ints = new TwoSum_167().twoSum(a, 9);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
