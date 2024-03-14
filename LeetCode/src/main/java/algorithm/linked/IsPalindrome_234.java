8c6483adbaaaa1b27dc815a1f0ada8669db401c5
Mylqsnh <jlthmylqsnh@163.com>
2024-03-14 23:51:57 +0800

    234 add solution

--- LeetCode/src/main/java/algorithm/linked/IsPalindrome_234.java
+++ LeetCode/src/main/java/algorithm/linked/IsPalindrome_234.java
@@ -4,12 +4,27 @@ import java.util.Arrays;
 
 public class IsPalindrome_234 {
     public static void main(String[] args) {
-        ListNode listNode = new ListNode(Arrays.asList(1, 2, 2, 1));
+        ListNode listNode = new ListNode(Arrays.asList(1, 2, 3, 2, 1));
         boolean palindrome = new IsPalindrome_234().isPalindrome(listNode);
         System.out.println(palindrome);
     }
 
+    ListNode left;
+
     public boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    public boolean traverse(ListNode head) {
+        if (head == null) return true;
+        boolean result = traverse(head.next);
+        result = result && left.val == head.val;
+        left = left.next;
+        return result;
+    }
+
+    public boolean isPalindrome2(ListNode head) {
         if (head == null) return false;
 
         ListNode fast = head, slow = head;
